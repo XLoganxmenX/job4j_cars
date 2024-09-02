de571a0fdae66aff6e5ada896af53e2ca21ac260
XLoganxmenX <loganxmen97@gmail.com>
2024-09-02 18:30:35 +0900

    fix 004 create table script

--- src/main/resources/db/scripts/004_ddl_create_table_price_history.sql
+++ src/main/resources/db/scripts/004_ddl_create_table_price_history.sql
@@ -1,7 +1,7 @@
 CREATE TABLE PRICE_HISTORY(
    id SERIAL PRIMARY KEY,
-   post_id references auto_post(id),
+   post_id int references auto_post(id),
    before BIGINT not null,
    after BIGINT not null,
-   created TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+   created TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
 );
