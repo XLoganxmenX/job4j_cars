5cb9935bb01b64c0ddca84548b70368a8b22b6de
XLoganxmenX <loganxmen97@gmail.com>
2024-09-02 17:24:24 +0900

    add CrudRepository, HibernateConfiguration, change logic of userRepository

--- src/main/java/ru/job4j/repository/HbmUserRepository.java
+++ src/main/java/ru/job4j/repository/HbmUserRepository.java
@@ -1,20 +1,17 @@
 package ru.job4j.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class HbmUserRepository implements UserRepository {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     /**
      * Сохранить в базе.
@@ -23,16 +20,7 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public User create(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.persist(user));
         return user;
     }
 
@@ -42,20 +30,7 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public void update(User user) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.createQuery("UPDATE User SET login = :fLogin, password = :fPassword WHERE id = :fId")
-                    .setParameter("fLogin", user.getLogin())
-                    .setParameter("fPassword", user.getPassword())
-                    .setParameter("fId", user.getId())
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.merge(user));
     }
 
     /**
@@ -64,18 +39,10 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public void delete(int userId) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.createQuery("DELETE User WHERE id = :fId")
-                    .setParameter("fId", userId)
-                    .executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(
+                "DELETE FROM User WHERE id = :fId",
+                Map.of("fId", userId)
+        );
     }
 
     /**
@@ -84,19 +51,7 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public List<User> findAllOrderById() {
-        List<User> users = new ArrayList<>();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("from User ORDER BY id", User.class);
-            users = query.getResultList();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return users;
+        return crudRepository.query("from User order by id asc", User.class);
     }
 
     /**
@@ -105,20 +60,10 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public Optional<User> findById(int userId) {
-        Optional<User> optionalUser = Optional.empty();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("from User WHERE id = :fId", User.class)
-                    .setParameter("fId", userId);
-            optionalUser = query.uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return optionalUser;
+        return crudRepository.optional(
+                "from User where id = :fId", User.class,
+                Map.of("fId", userId)
+        );
     }
 
     /**
@@ -128,20 +73,10 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public List<User> findByLikeLogin(String key) {
-        List<User> users = new ArrayList<>();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("from User WHERE login LIKE :fLogin", User.class)
-                    .setParameter("fLogin",  "%" + key + "%");
-            users = query.getResultList();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return users;
+        return crudRepository.query(
+                "from User where login like :fKey", User.class,
+                Map.of("fKey", "%" + key + "%")
+        );
     }
 
     /**
@@ -151,19 +86,9 @@ public class HbmUserRepository implements UserRepository {
      */
     @Override
     public Optional<User> findByLogin(String login) {
-        Optional<User> optionalUser = Optional.empty();
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Query<User> query = session.createQuery("from User WHERE login = :fLogin", User.class)
-                    .setParameter("fLogin", login);
-            optionalUser = query.uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return optionalUser;
+        return crudRepository.optional(
+                "from User where login = :fLogin", User.class,
+                Map.of("fLogin", login)
+        );
     }
 }
---
+++ src/test/java/ru/job4j/repository/HbmUserRepositoryTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.repository;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.job4j.Main;
+import ru.job4j.model.User;
+
+import java.time.LocalDateTime;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HbmUserRepositoryTest {
+    private static CrudRepository crudRepository;
+    private static UserRepository userRepository;
+
+    @BeforeAll
+    public static void init() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        SessionFactory sf = context.getBean(SessionFactory.class);
+        crudRepository = new CrudRepository(sf);
+        userRepository = new HbmUserRepository(crudRepository);
+    }
+
+    @BeforeEach
+    public void deleteAll() {
+        userRepository.findAllOrderById().forEach(user -> userRepository.delete(user.getId()));
+    }
+
+    @Test
+    public void whenSaveUserAndFindById() throws Exception {
+        var expectedUser = userRepository.create(new User(0, "login", "password"));
+        var actualUser = userRepository.findById(expectedUser.getId()).get();
+        assertThat(actualUser).isEqualTo(expectedUser);
+    }
+
+    @Test
+    public void whenDeleteUser() {
+        var savedUser = userRepository.create(new User(0, "login", "password"));
+        userRepository.delete(savedUser.getId());
+        var foundUser = userRepository.findById(savedUser.getId());
+        assertThat(foundUser).isEmpty();
+        assertThat(userRepository.findAllOrderById()).isEmpty();
+    }
+}
